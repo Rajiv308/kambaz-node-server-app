6f5fc1e0e493ec5867055d1ad7a904d6cd89a3ca
rpmenon <rajivpremnath308@gmail.com>
2025-12-07 23:21:54 -0500

    Fix CORS Issue

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const allowedOrigins = process.env.CLIENT_URL
       "http://localhost:3000",
       "http://localhost:3006",
       "https://kambaz-next-js-fa25-mon-ecru.vercel.app",
-      "https://kambaz-next-js-fa25-mon-git-a5-rajiv308s-projects.vercel.app",
+      "https://kambaz-next-js-fa25-mon-git-project-rajiv308s-projects.vercel.app",
     ];
 app.use(
   cors({
